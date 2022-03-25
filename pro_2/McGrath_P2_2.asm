8e24334d41c1c1c866665e85444ab371ade00646
Grace McGrath <gracemcgrath@Graces-MacBook-Air-2.local>
2022-03-25 14:27:25 -0400

    renaming

---
+++ pro_2/McGrath_P2_2.asm
@@ -0,0 +1,101 @@
+;;;;;;; P2 for QwikFlash board ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Use this template for Part 2 of Experiment 2
+;
+;;;;;;; Program hierarchy ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Mainline
+;   Initial
+;
+;;;;;;; Assembler directives ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        list  P=PIC18F4520, F=INHX32, C=160, N=0, ST=OFF, MM=OFF, R=DEC, X=ON
+        #include <P18F4520.inc>
+        __CONFIG  _CONFIG1H, _OSC_HS_1H  ;HS oscillator
+        __CONFIG  _CONFIG2L, _PWRT_ON_2L & _BOREN_ON_2L & _BORV_2_2L  ;Reset
+        __CONFIG  _CONFIG2H, _WDT_OFF_2H  ;Watchdog timer disabled
+        __CONFIG  _CONFIG3H, _CCP2MX_PORTC_3H  ;CCP2 to RC1 (rather than to RB3)
+        __CONFIG  _CONFIG4L, _LVP_OFF_4L & _XINST_OFF_4L  ;RB5 enabled for I/O
+        errorlevel -314, -315          ;Ignore lfsr messages
+
+;;;;;;; Variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        cblock  0x000                  ;Beginning of Access RAM
+        VAR_1                      ;Define variables as needed
+        endc
+
+;;;;;;; Macro definitions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+MOVLF   macro  literal,dest
+        movlw  literal
+        movwf  dest
+        endm
+
+
+
+;;;;;;; Vectors ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        org  0x0000                    ;Reset vector
+        nop 
+        goto  Mainline
+
+        org  0x0008                    ;High priority interrupt vector
+        goto  $                        ;Trap
+
+        org  0x0018                    ;Low priority interrupt vector
+        goto  $                        ;Trap
+
+;;;;;;; Mainline program ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+Mainline
+        rcall  Initial                 ;Initialize everything
+L1
+		bsf ADCON0, 1
+		PL1
+		btfsc ADCON0,1
+		bra PL1
+
+		bcf PORTC, RC0
+		bcf PIR1, SSPIF
+		MOVLF 0x21,SSPBUF
+		PL2
+		btfss PIR1, SSPIF				; check if SSPIF=1, if so, skip next line
+		bra PL2
+
+		bcf PIR1, SSPIF
+		movff ADRESH, SSPBUF
+		PL3
+		btfss PIR1, SSPIF				; check if SSPIF=1, if so, skip next line
+		bra PL3
+
+		bsf PORTC, RC0
+
+        bra	L1
+
+
+;;;;;;; Initial subroutine ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; This subroutine performs all initializations of variables and registers.
+
+Initial
+        MOVLF  B'10001110',ADCON1      ;Enable PORTA & PORTE digital I/O pins
+		MOVLF  B'00001100',ADCON2      ;Enable PORTA & PORTE digital I/O pins
+		MOVLF  B'00011101',ADCON0      ;Enable PORTA & PORTE digital I/O pins
+        MOVLF  B'11100001',TRISA       ;Set I/O for PORTA
+        MOVLF  B'11011100',TRISB       ;Set I/O for PORTB
+        MOVLF  B'11010000',TRISC       ;Set I/0 for PORTC
+        MOVLF  B'00001111',TRISD       ;Set I/O for PORTD
+        MOVLF  B'00000100',TRISE       ;Set I/O for PORTE
+        MOVLF  B'10001000',T0CON       ;Set up Timer0 for a looptime of 10 ms
+        MOVLF  B'00010000',PORTA       ;Turn off all four LEDs driven from PORTA
+		MOVLF  B'00100000',SSPCON1	   ;
+		MOVLF  B'11000000',SSPSTAT
+        return
+
+
+
+
+
+
+        end
+
